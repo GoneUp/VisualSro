d873888905a8491ef7e57dbe39f935736fa23243
GoneUp <GoneUp@129e5e74-3530-4cbc-a21f-c01795b42233>
2012-05-18 17:41:19 +0000

    - Fix

---
+++ Old VisualSro Project/Visual Silkroad - New/GameServer/Functions/Global/cGameObject.vb
@@ -0,0 +1,9 @@
+﻿Public Class cGameObject
+    Public Pk2ID As UInt32
+    Public UniqueID As UInt32
+    Public ChannelId As UInt32
+
+    Public Overrides Function ToString() As String
+        Return String.Format("GameObject, Pk2Id: {0}, UniqueId: {1}", Pk2ID, UniqueID)
+    End Function
+End Class
